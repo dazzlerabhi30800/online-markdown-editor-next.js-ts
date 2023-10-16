a2f1be18c32e500397f3fa1d0882a660ae70f44e
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-10-16 20:17:20 +0530

    added new custom component in page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,40 @@ import remarkGfm from "remark-gfm";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
 
+const customRenderers = {
+  code(code: any) {
+    const { node } = code;
+    const { lang, value } = node;
+    return (
+      <div>
+        <SyntaxHighlighter
+          style={atomDark}
+          language={lang}
+          children={value}
+        />
+      </div>
+    )
+  }
+}
+
+
+
+
 export default function Home() {
   const [value, setValue] = useState<string>("");
-
-
   return (
-    <div className="flex min-h-screen p-5">
+    <div className="flex min-h-[calc(100vh-63.6094px)] p-5">
       <textarea
         name="markdown"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        className="bg-slate-900 fullheight w-full relative outline-none text-white border-0 p-2 "
+        className="bg-slate-900 h-inherit w-full relative outline-none text-white border-0 p-2 "
       ></textarea>
-      {value.length > 0 &&
-        <div className="bg-slate-800 p-2 h-full w-full text-white editor">
-          <ReactMarkdown children={value} remarkPlugins={[remarkGfm]}
-          />
+      {value.length > 0 && (
+        <div className="bg-slate-800 p-2 h-inherit w-full text-white editor">
+          <ReactMarkdown children={value} components={customRenderers} remarkPlugins={[remarkGfm]} />
         </div>
-      }
-    </div >
+      )}
+    </div>
   );
 }
