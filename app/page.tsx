39a27e7537e4e94e14e401e4d1df4721adf2ce6a
= <abhisingh30800@gmail.com>
2023-10-17 19:58:12 +0530

    removed children from react markdown component

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,33 @@ import { FunctionComponent, useState, useEffect } from "react";
 import ReactMarkdown, { Components } from "react-markdown";
 import remarkGfm from 'remark-gfm';
 import Navbar from "@/Components/Navbar";
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
 
 
 
 
-// const customRenderers: any = {
-//   const({ node, inline, className, children, ...props }: any) {
-//     const { value } = node;
-//     const match = /language-(\w+)/.exec(className || "")
-//     return !inline && match ? (
-//       <SyntaxHighlighter
-//         // children={String(children).replace(/\n$/, '')}
-//         children={value}
-//         style={atomDark}
-//         language={match[1]}
-//         {...props}
-//       />
+const customRenderers: any = {
+  const({ node, inline, className, children, ...props }: any) {
+    const { value } = node;
+    const match = /language-(\w+)/.exec(className || "")
+    return !inline && match ? (
+      <SyntaxHighlighter
+        children={String(children).replace(/\n$/, '')}
+        style={atomDark}
+        language={match[1]}
+        {...props}
+      />
 
-//     )
-//       :
-//       (
-//         <code className={className} {...props}>
-//           {children}
-//         </code>
-//       )
-//   }
-// }
+    )
+      :
+      (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      )
+  }
+}
 
 
 const Home: FunctionComponent = () => {
@@ -43,16 +44,18 @@ const Home: FunctionComponent = () => {
   return (
     <main className="min-h-screen">
       <Navbar valueString={value} />
-      <div className="flex min-h-[calc(100vh-63.6094px)] p-5">
+      <div className="flex min-h-[calc(100vh-64px)] p-5">
         <textarea
           name="markdown"
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          className="bg-slate-900 h-inherit w-full relative outline-none text-white border-0 p-2 "
+          className="bg-slate-900 min-h-inherit w-full relative outline-none text-white border-0 p-2 "
         ></textarea>
         {value.length > 0 &&
-          <div className="bg-slate-800 p-2 h-inherit w-full text-white editor">
-            <ReactMarkdown children={value} remarkPlugins={[remarkGfm]} />
+          <div className="bg-slate-800 p-2 min-h-inherit w-full text-white editor">
+            <ReactMarkdown components={customRenderers} remarkPlugins={[remarkGfm]} >
+              {value}
+            </ReactMarkdown>
           </div>
         }
       </div>
