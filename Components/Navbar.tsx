2d167c1996264e41e7474628c3a9d5383442862b
= <abhisingh30800@gmail.com>
2023-10-17 20:17:16 +0530

    added copy to clipboard functionality

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 
 export default function Navbar({ valueString }: { valueString: string }) {
+  const [copy, setCopy] = useState<boolean>(false);
   const saveValueLocalStorage = () => {
     localStorage.setItem("value", valueString);
     alert('Saved');
   }
+
+  const copyValue = () => {
+    navigator.clipboard.writeText(valueString);
+    setCopy(prev => prev = true);
+    setTimeout(() => {
+      setCopy(prev => prev = false);
+    }, 2000)
+  }
   return (
     <nav className="nav">
       <h1>{"{M}"}Editor</h1>
-      <button onClick={saveValueLocalStorage} className="btn save">Save</button>
+      <div className="button--container">
+        <button onClick={copyValue} className={`btn copy ${copy && "copied"}`}>{copy ? "Copied to Clipboard" : "Copy to Clipboard"}</button>
+        <button onClick={saveValueLocalStorage} className="btn save">Save</button>
+      </div>
     </nav>
   );
 }
